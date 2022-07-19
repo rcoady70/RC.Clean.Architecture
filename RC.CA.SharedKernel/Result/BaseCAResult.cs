814769dcc6fda8e113d4ed70290bc62cafd8522f
Richard Coady <rcoady@gmail.com>
2022-07-19 11:34:28 +0100

    - Re factor CAResult to use empty result if no return - Re factor CSV and crud example to use CAResult

---
+++ RC.CA.SharedKernel/Result/BaseCAResult.cs
@@ -0,0 +1,9 @@
+﻿namespace RC.CA.SharedKernel.Result
+{
+    /// <summary>
+    /// Marker class. All dto's which are used in CAResult must inherit from here.
+    /// </summary>
+    public class BaseCAResult
+    {
+    }
+}
