eeb78d00e46bce547893c014833eaf25faa5841d
Richard Coady <rcoady@gmail.com>
2022-07-22 07:41:07 +0100

    Add unit test project

---
+++ RC.CA.WebApi.Test/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace RC.CA.WebApi.Test.Extensions;
+
+static class HttpClientExtensions
+{
+    public static HttpClient CreateIdempotentClient(this TestServer server)
+    {
+        var client = server.CreateClient();
+        client.DefaultRequestHeaders.Add("x-requestid", Guid.NewGuid().ToString());
+        return client;
+    }
+}
