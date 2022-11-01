0bbc3eea33051c8e9b6c05b7e045fac68e41fcb4
Richard Coady <rcoady@gmail.com>
2022-11-01 07:24:12 +0000

    Delegate to setup dbcontext

--- RC.CA.Infrastructure.Persistence/SetupDbContextService.cs
+++ RC.CA.Infrastructure.Persistence/SetupDbContextService.cs
@@ -1,34 +1,46 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using RC.CA.Application.Contracts.Persistence;
-using RC.CA.Infrastructure.Persistence.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
-using RC.CA.Application.Contracts.Identity;
+using RC.CA.Application.Contracts.Persistence;
+using RC.CA.Infrastructure.Persistence.Repository;
 
 namespace RC.CA.Infrastructure.Persistence;
 
 public static class SetupDbContextService
 {
+
+
+
     /// <summary>
     /// Setup application context, connection string etc
     /// </summary>
     /// <param name="services"></param>
     /// <param name="configuration"></param>
     /// <returns></returns>
-    public static IServiceCollection SetupDbContextServices(this IServiceCollection services, string connectionString,bool isDevelopment)
+    public static IServiceCollection SetupDbContextServices(this IServiceCollection services, string connectionString, bool isDevelopment)
     {
-        // Change database connection to be encapsulated in the db context
-        services.AddDbContext<ApplicationDbContext>(options =>
+        //Create configuration as a delegate
+        Action<DbContextOptionsBuilder> dbConfig = (options) =>
         {
             options.UseSqlServer(connectionString)
                    .UseLoggerFactory(CreateLoggerFactory(isDevelopment))
                    .EnableSensitiveDataLogging(isDevelopment)
-
                    //Set default tracking behavior to no tracking
                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        });
+        };
+        services.AddDbContext<ApplicationDbContext>((options) => dbConfig(options));
+
+        // Change database connection to be encapsulated in the db context
+        //services.AddDbContext<ApplicationDbContext>((options) =>
+        //{
+        //    options.UseSqlServer(connectionString)
+        //           .UseLoggerFactory(CreateLoggerFactory(isDevelopment))
+        //           .EnableSensitiveDataLogging(isDevelopment)
+
+        //           //Set default tracking behavior to no tracking
+        //           .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        //});
 
         //Generic repository
         services.TryAddScoped(typeof(IAsyncRepository<>), typeof(AsyncRepository<>));
@@ -46,10 +58,10 @@ public static class SetupDbContextService
     /// <returns></returns>
     private static ILoggerFactory CreateLoggerFactory(bool isDevelopment)
     {
-        if(isDevelopment)
-        return LoggerFactory.Create(builder => builder
-                            .AddFilter((category, level) => category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
-                            .AddConsole());
+        if (isDevelopment)
+            return LoggerFactory.Create(builder => builder
+                                .AddFilter((category, level) => category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
+                                .AddConsole());
         else
             return LoggerFactory.Create(builder => builder.AddFilter((category, level) => false));
     }
